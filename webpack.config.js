e6580ed235ac37638fc810e0b6e28367317ca9fb
Garrett Nay <garrettnay@ymail.com>
2016-02-19 06:59:55 -0700

    Add html-webpack-plugin to generate html

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 var path = require('path')
 
 module.exports = function () {
@@ -9,6 +10,15 @@ module.exports = function () {
     output: {
       filename: '[name].[hash].js',
       path: path.resolve('dist')
-    }
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: false,
+        template: 'node_modules/html-webpack-template/index.ejs',
+        appMountId: 'pacecalc',
+        title: 'Pacecalc'
+      })
+    ]
   }
 }
