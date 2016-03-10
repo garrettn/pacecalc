23b066c0f86530388e09fac694b98207c75dfa24
Garrett Nay <garrettnay@ymail.com>
2016-03-10 07:33:54 -0700

    Change naming convention

--- test/util/pace.test.js
+++ test/util/pace.test.js
@@ -6,7 +6,7 @@ import {
   renderSecondsString
 } from 'util/pace'
 
-test('util/pace/calculatePace', function (t) {
+test('util/pace.calculatePace', function (t) {
   const pace1 = calculatePace(
     { hours: 0, minutes: 30, seconds: 0 },
     3,
@@ -25,20 +25,20 @@ test('util/pace/calculatePace', function (t) {
   t.end()
 })
 
-test('util/pace/renderHoursString', function (t) {
+test('util/pace.renderHoursString', function (t) {
   t.equals(renderHoursString(0), '', 'renders empty string when hours is 0')
   t.equals(renderHoursString(2), '2:', 'renders hours with a colon')
   t.end()
 })
 
-test('util/pace/renderMinutesString', function (t) {
+test('util/pace.renderMinutesString', function (t) {
   t.equals(renderMinutesString(0, 0), '', 'renders empty string when hours and minutes are both 0')
   t.equals(renderMinutesString(0, 5), '5:', 'renders minutes with a colon')
   t.equals(renderMinutesString(1, 5), '05:', 'renders zeropadded minutes if hours > 0')
   t.end()
 })
 
-test('util/pace/renderSecondsString', function (t) {
+test('util/pace.renderSecondsString', function (t) {
   t.equals(renderSecondsString(0, 0, 15), '15 s', 'renders seconds with an s if hours and minutes are both 0')
   t.equals(renderSecondsString(0, 3, 15), '15', 'renders plain seconds if minutes > 0')
   t.equals(renderSecondsString(2, 0, 15), '15', 'renders plain seconds if hours > 0')
--- test/util/time.test.js
+++ test/util/time.test.js
@@ -1,7 +1,7 @@
 import test from 'tape'
 import { toSeconds, fromSeconds } from 'util/time'
 
-test('util/time/toSeconds', function (t) {
+test('util/time.toSeconds', function (t) {
   const totalSeconds = toSeconds({ hours: 2, minutes: 38, seconds: 45 })
   t.equals(totalSeconds, 9525, 'calculates total seconds')
 
@@ -10,7 +10,7 @@ test('util/time/toSeconds', function (t) {
   t.end()
 })
 
-test('util/time/fromSeconds', function (t) {
+test('util/time.fromSeconds', function (t) {
   const time = fromSeconds(9525)
   t.deepEquals(time, { hours: 2, minutes: 38, seconds: 45 }, 'splits total seconds')
   t.end()
